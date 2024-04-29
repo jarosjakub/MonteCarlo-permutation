2b2041ac9d57fbb5dcf674f0ac45952a6eb8facb
Jakub Jaroš <jarosj2@medtronic.com>
2024-04-29 10:28:34 +0200

    Add project files.

---
+++ Config.cs
@@ -0,0 +1,16 @@
+﻿namespace MonteCarlo;
+
+public static class Config
+{
+    // How many items do you want to choose from the base range
+    public static int Percentage = 5;
+
+    // Path to your input file
+    public static string InputPath = "C:\\Users\\jakub.jaros\\Desktop\\test.txt";
+
+    // Path to your output file
+    public static string OutputPath = "C:\\Users\\jakub.jaros\\Desktop\\output.txt";
+
+    // How to split the individual data
+    public static string Splitter = "\r\n";
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿namespace MonteCarlo;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var input = new List<int>();
+        var text = File.ReadAllText(Config.InputPath);
+        Console.WriteLine(text);
+        string[] words = text.Split(Config.Splitter);
+
+        foreach (string word in words)
+        {
+            int cislo = Int32.Parse(word);
+            input.Add(cislo);
+        }
+        Console.WriteLine(input[3] + input[5]);
+
+
+        Console.ReadLine();
+
+        //int cislo = Int32.Parse(input);
+
+
+
+
+
+        //var numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        //var random = new Random();
+        //int sum = 0;
+
+        //for (int i = 0; i < Config.Percentage; i++)
+        //{
+        //    var rng = random.Next(0, numbers.Count);
+        //    sum = sum + numbers[rng];
+        //    Console.WriteLine(numbers[rng]);
+        //    numbers.Remove(numbers[rng]);
+        //}
+
+
+        //Console.WriteLine(sum);
+
+
+
+
+
+
+
+
+
+    }
+}
